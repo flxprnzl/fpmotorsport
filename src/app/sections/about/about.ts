67ef33e471e9be2936ea6fa84d5a581d0623cb8a
Felix Prenzel <felix.prenzel@gmx.de>
2026-03-22 19:55:23 +0100

    update

--- src/app/sections/about/about.ts
+++ src/app/sections/about/about.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RevealOnScroll } from '../../shared/directives/reveal-on-scroll';
+
 @Component({
   selector: 'app-about',
-  imports: [RevealOnScroll],
+  standalone: true,
+  imports: [RevealOnScroll, CommonModule],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {}
+export class About {
+  isHovering = false;
+  cursorX = 0;
+  cursorY = 0;
+
+  onMouseMove(event: MouseEvent): void {
+    const rect = (event.currentTarget as HTMLElement).getBoundingClientRect();
+    this.cursorX = event.clientX - rect.left;
+    this.cursorY = event.clientY - rect.top;
+  }
+
+  onMouseEnter(): void {
+    this.isHovering = true;
+  }
+
+  onMouseLeave(): void {
+    this.isHovering = false;
+  }
+}
